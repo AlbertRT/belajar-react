c4b16dcd934aee338fe54d32552741b08b5846fe
Albert <87812438+AlbertRT@users.noreply.github.com>
2023-02-10 17:32:21 +0700

    React list & keys (render multiple components)

--- public/app.js
+++ public/app.js
@@ -1,16 +1,10 @@
 const root = document.querySelector("#root");
 function App() {
-  const [login, setLogin] = React.useState(false);
-  const judulRef = React.useRef(null);
-  React.useEffect(function () {
-    // const judul = document.getElementById("judul")
-    // setTimeout(function() {
-    //      judul.textContent = "Application";
-    // }, 1000)
-    judulRef.current.textContent = 'Aplikasi';
-  }, []);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
-    ref: judulRef
-  }, "Judul"));
+  const fruits = ['apel', 'lengkeng', 'jeruk'];
+  return /*#__PURE__*/React.createElement("ul", null, fruits.map(function (fruit) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: fruit
+    }, fruit);
+  }));
 }
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,22 +1,18 @@
 const root = document.querySelector("#root");
 
 function App() {
-  const [login, setLogin] = React.useState(false);
-  const judulRef = React.useRef(null)
-
-  React.useEffect(function() {
-     // const judul = document.getElementById("judul")
-     // setTimeout(function() {
-     //      judul.textContent = "Application";
-     // }, 1000)
-     judulRef.current.textContent = 'Aplikasi'
-  }, [])
-
-  return (
-    <>
-      <h1 ref={judulRef}>Judul</h1>
-    </>
-  );
+     const fruits = [
+          'apel',
+          'lengkeng',
+          'jeruk'
+     ]
+  return(
+     <ul>
+          {fruits.map(function(fruit) {
+               return <li key={fruit}>{fruit}</li>
+          })}
+     </ul>
+  )
 }
 
 ReactDOM.render(<App />, root);

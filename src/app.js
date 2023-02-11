3ca61483b9dc683d105d550b25283560d3f7c694
Albert <87812438+AlbertRT@users.noreply.github.com>
2023-02-11 16:30:25 +0700

    Sync data fetching response with UI

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const root = document.querySelector("#root");
 
 function App() {
+     const [news, setNews] = React.useState([])
+     const [loading, setLoading] = React.useState(true)
 
      React.useEffect(function () {
           async function getData() {
@@ -8,13 +10,25 @@ function App() {
                  "https://api.spaceflightnewsapi.net/v3/blogs"
                );
                const response = await request.json()
-
-               console.log(request)
+               
+               setNews(response)
+               setLoading(false)
           }
           getData()
      }, []);
 
-     return <h1>Data Fetch</h1>
+     return (
+       <>
+         <h1>Data Fetch</h1>
+         {loading ? <i>Loading data...</i> : (
+           <ul>
+             {news.map(function (item) {
+               return <li key={item.id}>{item.title}</li>;
+             })}
+           </ul>
+         )}
+       </>
+     );
 }
 
 ReactDOM.render(<App />, root);
